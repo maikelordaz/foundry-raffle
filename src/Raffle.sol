4cf4666f6435a51577fa4fb19e7f92656ad6820c
Maikel Ordaz <maikelordaz@gmail.com>
2024-03-14 16:06:31 -0400

    enter raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -7,13 +7,54 @@ pragma solidity ^0.8.19;
  * @notice Sample Raffle contract using foundry
  */
 contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+    error Raffle__RaffleNotOpen();
+
+    /* Type declarations */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+    RaffleState private s_raffleState;
+
+    event RaffleEnter(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
+        s_raffleState = RaffleState.OPEN;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
+        }
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEnter(msg.sender);
+    }
+
+    /** Getter Functions */
+
+    function getRaffleState() public view returns (RaffleState) {
+        return s_raffleState;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
     }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
     }
 }
