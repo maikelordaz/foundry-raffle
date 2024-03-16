ce30100723fdb40bf2a6c4b0d0f5b8fa57ef1be8
Maikel Ordaz <maikelordaz@gmail.com>
2024-03-15 21:45:37 -0400

    Update DeployRaffle.s.sol

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,7 +4,23 @@ pragma solidity 0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRaffle is Script {
-    function run() external returns (Raffle) {}
+    function run() external returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            uint64 subscriptionId,
+            bytes32 gasLane,
+            uint256 interval,
+            uint256 entranceFee,
+            uint32 callbackGasLimit,
+            address vrfCoordinatorV2
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(subscriptionId, gasLane, interval, entranceFee, callbackGasLimit, vrfCoordinatorV2);
+        vm.stopBroadcast();
+        return (raffle, helperConfig);
+    }
 }
